24e4f7c434fc65d780f13532f73f4c76389dd7b2
Boris Ning <fartbagxp@gmail.com>
2016-06-03 21:38:19 -0400

    Fixing travis.yml crashes

--- config.js
+++ config.js
@@ -1,8 +1,18 @@
 'use strict';
 
+const _ = require('lodash');
+
 const config = {
   host: process.env.SERVER_HOST,
   port: process.env.SERVER_PORT
 };
+
+if(_.isUndefined(config.host)) {
+  config.host = '127.0.0.1';
+}
+
+if(_.isUndefined(config.port)) {
+  config.port = 8080;
+}
 
 module.exports = config;
--- src/spec/server.spec.js
+++ src/spec/server.spec.js
@@ -1,7 +1,9 @@
 'use strict';
 
 const async = require('async');
+const config = require('../../config');
 const server = require('../server');
+const request = require('superagent');
 const expect = require('chai').expect;
 
 describe('Server setup testing.', function () {
@@ -10,8 +12,21 @@ describe('Server setup testing.', function () {
 
       const steps = [];
 
+      const uri = config.host + ':' + config.port;
+
       steps.push(function(cb) {
         server.start(cb);
+      });
+
+      steps.push(function(cb) {
+        request
+          .get(uri + '/hello')
+          .end(function(err, res){
+            expect(err).to.be.null;
+            expect(res.status).to.equal(200);
+            expect(res.text).to.equal('Hello World!');
+            cb && cb(err);
+          });
       });
 
       steps.push(function(cb) {
